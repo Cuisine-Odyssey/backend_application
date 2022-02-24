061c9766f3c41c293c74ef3a4d2d8e7f48daf6a1
David Kassin <david.j.kassin@gmail.com>
2022-02-24 16:38:41 -0700

    Fixed setup issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_000653) do
+ActiveRecord::Schema.define(version: 2022_02_24_233411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2022_02_24_000653) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
