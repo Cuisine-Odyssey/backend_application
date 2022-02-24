c0baffb6e4294559eee378f933207ae22b5d527b
David Kassin <david.j.kassin@gmail.com>
2022-02-24 16:37:53 -0700

    Fixed tests to remove bcrypt

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
-    #password_confirmation { password }
+
   end
 end
