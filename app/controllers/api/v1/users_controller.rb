624f5f3241339d865cc0aad1a42f05cff26cf8a1
David Kassin <david.j.kassin@gmail.com>
2022-02-24 17:44:02 -0700

    add find_or_create_by to users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,21 @@
 class Api::V1::UsersController < ApplicationController
+
   def show
-    if User.exists?(params[:id])
-      render json: UserSerializer.new(User.find(params[:id]))
+    new_user = User.find_or_create_by(user_params) do |user|
+                  user.email = params[:email]
+                  user.first_name = params[:first_name]
+                  user.last_name = params[:last_name]
+    end
+    if new_user.present?
+      render json: UserSerializer.new(new_user)
     else
       render status: 404
     end
+  end
+
+private
+
+  def user_params
+    params.permit(:email, :first_name, :last_name)
   end
 end
